a6d24944039f9cb25712d95cf0b95da62111be87
xabyer <xabyer.b@gmail.com>
2025-09-24 13:49:22 +0200

    ProcessBuilder Tarea PipeLine terminada.

--- UT1-Programacion_Multiproceso/Ejercicios/PipeLine/Pipeline/src/main/java/com/github/xabyer/dam/psp/Main.java
+++ UT1-Programacion_Multiproceso/Ejercicios/PipeLine/Pipeline/src/main/java/com/github/xabyer/dam/psp/Main.java
@@ -1,4 +1,9 @@
 package com.github.xabyer.dam.psp;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Lanza tres procesos a la vez en modo "pipeline", es decir, cada uno de ellos (salvo el primero) debe tomar como entrada la salida del proceso anterior. Los procesos son:
 
@@ -14,9 +19,25 @@ public class Main {
         Nota: Leer el ProcessBuilder-Pipeline.md
      */
     static void main() {
+        //1 Lista ProcessBuilder y los processBuilder a añadir:
+        List<ProcessBuilder> processBuilderList = new ArrayList<>();
         ProcessBuilder cmdProcessBuilder = new ProcessBuilder("cmd", "/c", "set");
         ProcessBuilder findStrProcessBuilder = new ProcessBuilder("findstr", "USER");
         ProcessBuilder sortProcessBuilder = new ProcessBuilder("sort");
+
+        // 2. Redirección de la salida del último proceso
+        sortProcessBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+        // 3. Añadir procesos a la lista:
+        processBuilderList.add(cmdProcessBuilder);
+        processBuilderList.add(findStrProcessBuilder);
+        processBuilderList.add(sortProcessBuilder);
+
+        try {
+            var processPipeList = ProcessBuilder.startPipeline(processBuilderList);
+        } catch (IOException e) {
+            System.err.println("Error lanzando procesos: " + e.getMessage());
+        }
 
 
     }
