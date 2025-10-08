455f9b79f157b080b6b7268ea563385cd4fa7d1e
xabyer <xabyer.b@gmail.com>
2025-10-08 22:45:52 +0200

    Pipeline2 añadida versión para hacer el pipeline mediante streams.

--- UT1-Programacion_Multiproceso/PreparacionExamen/EjerciciosPropios/Pipeline2/Pipeline2/src/main/java/Main2.java
+++ UT1-Programacion_Multiproceso/PreparacionExamen/EjerciciosPropios/Pipeline2/Pipeline2/src/main/java/Main2.java
@@ -5,10 +5,16 @@ heredandose del proceso padre.
  */
 
 void main() {
-    startPipeLineProcessBuilder();
+    try {
+        startPipeLineProcessBuilder();
+    } catch (IOException e) {
+        e.printStackTrace(System.err);
+    }
 }
 
-private void startPipeLineProcessBuilder() {
+private void startPipeLineProcessBuilder() throws IOException {
+    final String CHARSET = "UTF-8";
+
     ProcessBuilder dirProcessBuilder = new ProcessBuilder("CMD", "/C", "DIR", "/W");
     ProcessBuilder sortProcessBuilder = new ProcessBuilder("SORT", "/r");
 
@@ -16,9 +22,25 @@ private void startPipeLineProcessBuilder() {
 
     sortProcessBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
 
+    Process dirProcess = dirProcessBuilder.start();
+    Process sortProcess = sortProcessBuilder.start();
+
     // 1. Redirigir la salida del proceso dir a la entrada del proceso Sort:
+
+    try(
+        BufferedReader mainInputStream = new BufferedReader(new InputStreamReader(dirProcess.getInputStream(), CHARSET));
+        BufferedWriter mainOutputStream = new BufferedWriter(new OutputStreamWriter(sortProcess.getOutputStream(), CHARSET))
+    ){
+        String line;
+        while ( (line = mainInputStream.readLine()) != null) {
+            mainOutputStream.write(line);
+            mainOutputStream.newLine();
+        }
+    }
+
+
 }
 
 private String workingDirectory() {
-    return IO.readln("Introduce el directorio de trabajo");
+    return IO.readln("Introduce el directorio de trabajo: ");
 }
